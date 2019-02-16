ab5a7fc6f84be4d655aa45a82aef7865c3484270
Andrea <togniand@gmail.com>
2019-02-16 15:06:47 +0100

    Converted int to int64

--- database/structs.go
+++ database/structs.go
@@ -2,16 +2,16 @@ package database
 
 //User represent the respective table in the database
 type User struct {
-	ID        int
+	ID        int64
 	Nickname  string
 	Biography string
-	Status    int
+	Status    int64
 	LastSeen  string
 }
 
 //Group represent the respective table in the database
 type Group struct {
-	ID     int
+	ID     int64
 	Title  string
 	Ref    string
 	Locale string
@@ -19,82 +19,83 @@ type Group struct {
 
 //Channel represent the respective table in the database
 type Channel struct {
-	ID      int
-	GroupID int
+	ID      int64
+	GroupID int64
 	Name    string
 	Ref     string
 }
 
 //Permission represent the respective table in the database
 type Permission struct {
-	ID         int
-	UserID     int
-	GroupID    int
-	Permission int
+	ID         int64
+	UserID     int64
+	GroupID    int64
+	Permission int64
 }
 
 //List represent the respective table in the database
 type List struct {
-	ID               int
+	ID               int64
 	Name             string
-	GroupID          int
-	GroupIndipendent int
-	InviteOnly       int
+	GroupID          int64
+	GroupIndipendent int64
+	InviteOnly       int64
+	status           int64
 }
 
 //Bookmark represent the respective table in the database
 type Bookmark struct {
-	ID             int
-	UserID         int
-	GroupID        int
-	MessageID      int
+	ID             int64
+	UserID         int64
+	GroupID        int64
+	MessageID      int64
 	Alias          string
-	Status         int
+	Status         int64
 	MessageContent string
 }
 
 //Subscription represent the respective table in the database
 type Subscription struct {
-	ID     int
-	ListID int
-	UserID int
+	ID     int64
+	ListID int64
+	UserID int64
 }
 
 //MessageCount represent the respective table in the database
 type MessageCount struct {
-	ID           int
-	UserID       int
-	GroupID      int
-	MessageCount int
+	ID           int64
+	UserID       int64
+	GroupID      int64
+	MessageCount int64
 }
 
 //Strings represent the respective table in the database
 type Strings struct {
-	ID      int
+	ID      int64
 	Key     string
 	Value   string
 	Locale  string
-	GroupID int
+	GroupID int64
 }
 
 //Settings represent the respective table in the database
 type Settings struct {
-	ID      int
+	ID      int64
 	Key     string
 	Value   string
-	GroupID int
+	GroupID int64
 }
 
 //BotSettings represent the respective table in the database
 type BotSettings struct {
-	ID    int
+	ID    int64
 	Key   string
 	Value string
 }
 
 //BotString represent the respective table in the database
 type BotString struct {
-	ID     int
+	ID     int64
 	Key    string
 	Value  string
 	Locale string
@@ -102,19 +103,19 @@ type BotString struct {
 
 //Log represent the respective table in the database
 type Log struct {
-	ID             int
+	ID             int64
 	Event          string
-	RelatedUserID  int
-	RelatedGroupID int
+	RelatedUserID  int64
+	RelatedGroupID int64
 	Message        string
 	UpdateValue    string
 	Error          string
-	Severity       int
+	Severity       int64
 	Date           string
 }
 
 //BotAdministrator represent the respective table in the database
 type BotAdministrator struct {
-	UserID      int
-	Permissions int
+	UserID      int64
+	Permissions int64
 }
