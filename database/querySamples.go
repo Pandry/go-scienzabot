2cccbef679c2d851448b4b0d1e19a730d056606b
Andrea <togniand@gmail.com>
2019-02-20 12:52:47 +0100

    Improved sample query

--- database/querySamples.go
+++ database/querySamples.go
@@ -1,124 +1,99 @@
 package database
 
-import "errors"
+import (
+	"database/sql"
+	"errors"
+)
 
 func (db *SQLiteDB) insertQuery() error {
-	stmt, err := db.Prepare("INSERT INTO VALUES (?,?,?)")
+	query, err := db.Exec("INSERT INTO Table (`a`, `b`, `c`) VALUES (?,?,?)", "")
 	if err != nil {
-		db.AddLogEvent(Log{Event: "_QueryFailed", Message: "Impossible to create the  preparation query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "_QueryFailed", Message: "Impossible to create the execute the query", Error: err.Error()})
 		return err
 	}
-	defer stmt.Close()
-
-	res, err := stmt.Exec()
-	if err != nil {
-		db.AddLogEvent(Log{Event: "_ExecutionQueryFailed", Message: "Impossible to execute the  query", Error: err.Error()})
-		return err
-	}
-
-	rows, err := res.RowsAffected()
-
+	rows, err := query.RowsAffected()
 	if err != nil {
 		db.AddLogEvent(Log{Event: "_RowsInfoNotGot", Message: "Impossible to get afftected rows", Error: err.Error()})
 		return err
 	}
-
 	if rows < 1 {
 		db.AddLogEvent(Log{Event: "_NoRowsAffected", Message: "No rows affected", Error: err.Error()})
 		return NoRowsAffected{error: errors.New("No rows affected from the query")}
 	}
-
 	return err
 }
 
 func (db *SQLiteDB) deleteQuery() error {
-	stmt, err := db.Prepare("DELETE FROM Table WHERE `ID=?`")
+	query, err := db.Exec("DELETE FROM Table WHERE a=?", "a")
 	if err != nil {
-		db.AddLogEvent(Log{Event: "_QueryFailed", Message: "Impossible to create the  preparation query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "_QueryFailed", Message: "Impossible to create the execute the query", Error: err.Error()})
 		return err
 	}
-	defer stmt.Close()
-
-	res, err := stmt.Exec()
-	if err != nil {
-		db.AddLogEvent(Log{Event: "_ExecutionQueryFailed", Message: "Impossible to execute the  query", Error: err.Error()})
-		return err
-	}
-
-	rows, err := res.RowsAffected()
-
+	rows, err := query.RowsAffected()
 	if err != nil {
 		db.AddLogEvent(Log{Event: "_RowsInfoNotGot", Message: "Impossible to get afftected rows", Error: err.Error()})
 		return err
 	}
-
 	if rows < 1 {
 		db.AddLogEvent(Log{Event: "_NoRowsAffected", Message: "No rows affected", Error: err.Error()})
 		return NoRowsAffected{error: errors.New("No rows affected from the query")}
 	}
-
 	return err
 }
 
 func (db *SQLiteDB) updateQuery() error {
-	stmt, err := db.Prepare("UPDATE Table SET `Field`=?, `Field2`=? WHERE `ID`=?")
+	query, err := db.Exec("UPDATE Table SET `Field`=?, `Field2`=? WHERE `ID`=?", "a", "", "")
 	if err != nil {
-		db.AddLogEvent(Log{Event: "_QueryFailed", Message: "Impossible to create the  preparation query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "_QueryFailed", Message: "Impossible to create the execute the query", Error: err.Error()})
 		return err
 	}
-	defer stmt.Close()
-
-	res, err := stmt.Exec()
-	if err != nil {
-		db.AddLogEvent(Log{Event: "_ExecutionQueryFailed", Message: "Impossible to execute the  query", Error: err.Error()})
-		return err
-	}
-
-	rows, err := res.RowsAffected()
-
+	rows, err := query.RowsAffected()
 	if err != nil {
 		db.AddLogEvent(Log{Event: "_RowsInfoNotGot", Message: "Impossible to get afftected rows", Error: err.Error()})
 		return err
 	}
-
 	if rows < 1 {
 		db.AddLogEvent(Log{Event: "_NoRowsAffected", Message: "No rows affected", Error: err.Error()})
 		return NoRowsAffected{error: errors.New("No rows affected from the query")}
 	}
-
 	return err
 }
 
+func (db *SQLiteDB) selectSingleQuery() error {
+	var (
+		name string
+		Age  sql.NullInt64
+	)
+	err := db.QueryRow("SELECT * FROM Table WHERE `ID`=?", "a").Scan(&name, &Age)
+	switch {
+	case err == sql.ErrNoRows:
+		db.AddLogEvent(Log{Event: "_ErrorNoRows", Message: "Impossible to get rows", Error: err.Error()})
+		return err
+	case err != nil:
+		db.AddLogEvent(Log{Event: "_ErrorUnknown", Message: "Uknown error verified", Error: err.Error()})
+		return err
+	default:
+		return nil
+	}
+}
+
 func (db *SQLiteDB) selectMultipleQuery() error {
-	stmt, err := db.Prepare("SELECT * FROM Table WHERE `ID`=?")
+	query, err := db.Query("SELECT * FROM Table WHERE `a`=?", "a")
+	defer query.Close()
 	if err != nil {
-		db.AddLogEvent(Log{Event: "_QueryFailed", Message: "Impossible to create the  preparation query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "_ErorExecutingTheQuery", Message: "Impossible to get afftected rows", Error: err.Error()})
 		return err
 	}
-	defer stmt.Close()
-
-	res, err := stmt.Query()
-	if err != nil {
-		db.AddLogEvent(Log{Event: "_ExecutionQueryFailed", Message: "Impossible to execute the  query", Error: err.Error()})
-		return err
-	}
-
-	err = res.Err()
-
-	if err != nil {
-		db.AddLogEvent(Log{Event: "_SelectQueryFailed", Message: "Impossible to execute the query: an error verified", Error: err.Error()})
-		return err
-	}
-
 	strings := make([]string, 0)
-	for res.Next() {
+	for query.Next() {
 		var name string
-
-		if err := res.Scan(&name); err != nil {
+		if err := query.Scan(&name); err != nil {
 			db.AddLogEvent(Log{Event: "_RowQueryFetchResultFailed", Message: "Impossible to get data from the row", Error: err.Error()})
 		}
-
 		strings = append(strings, name)
+	}
+	if !query.NextResultSet() {
+		db.AddLogEvent(Log{Event: "_RowNotFetched", Message: "Some rows in the query were not fetched", Error: err.Error()})
 
 	}
 
