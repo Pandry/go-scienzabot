b67ca9f9348c6a15423bae97c8f782cbf0c0e14e
Andrea <togniand@gmail.com>
2019-02-20 13:39:40 +0100

    refactored the bookmarksHelpers queries

--- database/bookmarksHelpers.go
+++ database/bookmarksHelpers.go
@@ -1,6 +1,8 @@
 package database
 
-import "database/sql"
+import (
+	"errors"
+)
 
 /*
 type Bookmark struct {
@@ -17,171 +19,150 @@ type Bookmark struct {
 //CreateBookmark takes a database.Bookmark struct as parameter and insert it in the database
 //The ID will not be considered, since it's automatically inrted in database. ALl the other values will be inserted
 func (db *SQLiteDB) CreateBookmark(bkm Bookmark) error {
-	stmt, err := db.Prepare("INSERT INTO Bookmarks (`UserID`, `GroupID`, `MessageID`, `Alias`, `Status`, `MessageContent`) VALUES (?,?,?,?,?,?)")
+	query, err := db.Exec("INSERT INTO Bookmarks (`UserID`, `GroupID`, `MessageID`, `Alias`, `Status`, `MessageContent`) VALUES (?,?,?,?,?,?)",
+		bkm.UserID, bkm.UserID, bkm.GroupID, bkm.MessageID, bkm.Alias, bkm.Status, bkm.MessageContent)
 	if err != nil {
-		//Log the error
-		db.AddLogEvent(Log{Event: "CreateBookmark_QueryFailed", Message: "Impossible to create the CreateBookmark preparation query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "CreateBookmark_QueryFailed", Message: "Impossible to create the execute the query", Error: err.Error()})
 		return err
 	}
-	defer stmt.Close()
-
-	//And we execute it passing the parameters
-	_, err = stmt.Exec(bkm.UserID, bkm.UserID, bkm.GroupID, bkm.MessageID, bkm.Alias, bkm.Status, bkm.MessageContent)
+	rows, err := query.RowsAffected()
 	if err != nil {
-		db.AddLogEvent(Log{Event: "CreateBookmark_ExecutionQueryFailed", Message: "Impossible to execute the CreateBookmark query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "CreateBookmark_RowsInfoNotGot", Message: "Impossible to get afftected rows", Error: err.Error()})
+		return err
+	}
+	if rows < 1 {
+		db.AddLogEvent(Log{Event: "CreateBookmark_NoRowsAffected", Message: "No rows affected", Error: err.Error()})
+		return NoRowsAffected{error: errors.New("No rows affected from the query")}
 	}
 	return err
 }
 
 //DeleteBookmark takes a Bookmark ID and deletes it
 func (db *SQLiteDB) DeleteBookmark(bkmID int) error {
-	stmt, err := db.Prepare("DELETE FROM Bookmarks WHERE `ID` = ?")
+	query, err := db.Exec("DELETE FROM Bookmarks WHERE `ID` = ?", bkmID)
 	if err != nil {
-		//Log the error
-		db.AddLogEvent(Log{Event: "DeleteBookmark_QueryFailed", Message: "Impossible to create the DeleteBookmark preparation query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "DeleteBookmark_QueryFailed", Message: "Impossible to create the execute the query", Error: err.Error()})
 		return err
 	}
-	defer stmt.Close()
-
-	//And we execute it passing the parameters
-	_, err = stmt.Exec(bkmID)
+	rows, err := query.RowsAffected()
 	if err != nil {
-		db.AddLogEvent(Log{Event: "DeleteBookmark_ExecutionQueryFailed", Message: "Impossible to execute the DeleteBookmark query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "DeleteBookmark_RowsInfoNotGot", Message: "Impossible to get afftected rows", Error: err.Error()})
+		return err
+	}
+	if rows < 1 {
+		db.AddLogEvent(Log{Event: "DeleteBookmark_NoRowsAffected", Message: "No rows affected", Error: err.Error()})
+		return NoRowsAffected{error: errors.New("No rows affected from the query")}
 	}
 	return err
 }
 
 //RenameBookmark takes a Bookmark ID and a string and renames  a bookmark
 func (db *SQLiteDB) RenameBookmark(bkmID int, newAlias string) error {
-	stmt, err := db.Prepare("UPDATE Bookmarks SET `Alias`=? WHERE `ID` = ?")
+	query, err := db.Exec("UPDATE Bookmarks SET `Alias`=? WHERE `ID` = ?", newAlias, bkmID)
 	if err != nil {
-		//Log the error
-		db.AddLogEvent(Log{Event: "RenameBookmark_QueryFailed", Message: "Impossible to create the RenameBookmark preparation query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "RenameBookmark_QueryFailed", Message: "Impossible to create the execute the query", Error: err.Error()})
 		return err
 	}
-	defer stmt.Close()
-
-	//And we execute it passing the parameters
-	_, err = stmt.Exec(newAlias, bkmID)
+	rows, err := query.RowsAffected()
 	if err != nil {
-		db.AddLogEvent(Log{Event: "RenameBookmark_ExecutionQueryFailed", Message: "Impossible to execute the RenameBookmark query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "RenameBookmark_RowsInfoNotGot", Message: "Impossible to get afftected rows", Error: err.Error()})
+		return err
+	}
+	if rows < 1 {
+		db.AddLogEvent(Log{Event: "RenameBookmark_NoRowsAffected", Message: "No rows affected", Error: err.Error()})
+		return NoRowsAffected{error: errors.New("No rows affected from the query")}
 	}
 	return err
 }
 
 //GetAllBookmarks returns all the bookmark in the database
 func (db *SQLiteDB) GetAllBookmarks() ([]Bookmark, error) {
-	stmt, err := db.Prepare("SELECT `ID`, `UserID`, `GroupID`, `MessageID`, `Alias`, `Status`, `MessageContent` FROM Bookmarks")
+	rows, err := db.Query("SELECT `ID`, `UserID`, `GroupID`, `MessageID`, `Alias`, `Status`, `MessageContent` FROM Bookmarks")
+	defer rows.Close()
 	if err != nil {
-		//Log the error
-		db.AddLogEvent(Log{Event: "RenameBookmark_QueryFailed", Message: "Impossible to create the RenameBookmark preparation query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "GetAllBookmarks_ErorExecutingTheQuery", Message: "Impossible to get afftected rows", Error: err.Error()})
 		return nil, err
 	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query()
-
-	switch {
-	case err == sql.ErrNoRows:
-		//User does not exist
-		db.AddLogEvent(Log{Event: "RequestedNickname_DontExists", Message: "Requested a nickname not present in the database", Error: err.Error()})
-		return nil, err
-
-	case err != nil:
-		db.AddLogEvent(Log{Event: "RequestedNickname_DontExistsUnknown", Message: "Requested a nickname not present in the database but the error is unknown", Error: err.Error()})
-		return nil, err
-	}
-
-	var bkms []Bookmark
-
-	var id, userID, groupID, messageID, status int64
-	var messageContent, alias string
+	bkms := make([]Bookmark, 0)
 	for rows.Next() {
-		err = rows.Scan(&id, &userID, &groupID, &messageID, &alias, &status, &messageContent)
-		//And check for errors
-		if err != nil {
-			db.AddLogEvent(Log{Event: "GetAllBookmarks_QueryScanError", Message: "An error verified while scanning a result row", Error: err.Error()})
+		var (
+			id, userID, groupID, messageID, status int64
+			messageContent, alias                  string
+		)
+		if err = rows.Scan(&id, &userID, &groupID, &messageID, &alias, &status, &messageContent); err != nil {
+			db.AddLogEvent(Log{Event: "GetAllBookmarks_RowQueryFetchResultFailed", Message: "Impossible to get data from the row", Error: err.Error()})
 		} else {
 			bkms = append(bkms, Bookmark{ID: id, UserID: userID, GroupID: groupID, MessageID: messageID, Alias: alias, Status: status, MessageContent: messageContent})
 		}
 	}
+	if !rows.NextResultSet() {
+		db.AddLogEvent(Log{Event: "GetAllBookmarks_RowNotFetched", Message: "Some rows in the query were not fetched", Error: err.Error()})
+	}
+	if err := rows.Err(); err != nil {
+		db.AddLogEvent(Log{Event: "GetAllBookmarks_UnknowQueryError", Message: "An unknown error was thrown", Error: err.Error()})
+	}
+
 	return bkms, err
 }
 
 //GetUserBookmarks returns all the bookmarks of a user
 func (db *SQLiteDB) GetUserBookmarks(iUserID int64) ([]Bookmark, error) {
-	stmt, err := db.Prepare("SELECT `ID`, `UserID`, `GroupID`, `MessageID`, `Alias`, `Status`, `MessageContent` FROM Bookmarks WHERE `UserID`=?")
+	rows, err := db.Query("SELECT `ID`, `UserID`, `GroupID`, `MessageID`, `Alias`, `Status`, `MessageContent` FROM Bookmarks WHERE `UserID`=?", iUserID)
+	defer rows.Close()
 	if err != nil {
-		//Log the error
-		db.AddLogEvent(Log{Event: "GetUserBookmarks_QueryFailed", Message: "Impossible to create the GetUserBookmarks preparation query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "GetUserBookmarks_ErorExecutingTheQuery", Message: "Impossible to get afftected rows", Error: err.Error()})
 		return nil, err
 	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(iUserID)
-
-	switch {
-	case err == sql.ErrNoRows:
-		//User does not exist
-		db.AddLogEvent(Log{Event: "GetUserBookmarks_DontExists", Message: "Requested a nickname not present in the database", Error: err.Error()})
-		return nil, err
-
-	case err != nil:
-		db.AddLogEvent(Log{Event: "GetUserBookmarks_DontExistsUnknown", Message: "Requested a nickname not present in the database but the error is unknown", Error: err.Error()})
-		return nil, err
-	}
-
-	var bkms []Bookmark
-
-	var id, userID, groupID, messageID, status int64
-	var messageContent, alias string
+	bkms := make([]Bookmark, 0)
 	for rows.Next() {
-		err = rows.Scan(&id, &userID, &groupID, &messageID, &alias, &status, &messageContent)
-		//And check for errors
-		if err != nil {
-			db.AddLogEvent(Log{Event: "GetUserBookmarks_QueryScanError", Message: "An error verified while scanning a result row", Error: err.Error()})
+		var (
+			id, userID, groupID, messageID, status int64
+			messageContent, alias                  string
+		)
+		if err = rows.Scan(&id, &userID, &groupID, &messageID, &alias, &status, &messageContent); err != nil {
+			db.AddLogEvent(Log{Event: "GetUserBookmarks_RowQueryFetchResultFailed", Message: "Impossible to get data from the row", Error: err.Error()})
 		} else {
 			bkms = append(bkms, Bookmark{ID: id, UserID: userID, GroupID: groupID, MessageID: messageID, Alias: alias, Status: status, MessageContent: messageContent})
 		}
 	}
+	if !rows.NextResultSet() {
+		db.AddLogEvent(Log{Event: "GetUserBookmarks_RowNotFetched", Message: "Some rows in the query were not fetched", Error: err.Error()})
+	}
+	if err := rows.Err(); err != nil {
+		db.AddLogEvent(Log{Event: "GetUserBookmarks_UnknowQueryError", Message: "An unknown error was thrown", Error: err.Error()})
+	}
+
 	return bkms, err
+
 }
 
 //GetUserGroupBookmarks returns the bookmarks of a user in a given
 func (db *SQLiteDB) GetUserGroupBookmarks(iUserID int64, iGroupID int64) ([]Bookmark, error) {
-	stmt, err := db.Prepare("SELECT `ID`, `UserID`, `GroupID`, `MessageID`, `Alias`, `Status`, `MessageContent` FROM Bookmarks WHERE `UserID`=? AND `GroupID`=?")
+
+	rows, err := db.Query("SELECT `ID`, `UserID`, `GroupID`, `MessageID`, `Alias`, `Status`, `MessageContent` FROM Bookmarks WHERE `UserID`=? AND `GroupID`=?", iUserID, iGroupID)
+	defer rows.Close()
 	if err != nil {
-		//Log the error
-		db.AddLogEvent(Log{Event: "GetUserGroupBookmarks_QueryFailed", Message: "Impossible to create the GetUserGroupBookmarks preparation query", Error: err.Error()})
+		db.AddLogEvent(Log{Event: "GetUserGroupBookmarks_ErorExecutingTheQuery", Message: "Impossible to get afftected rows", Error: err.Error()})
 		return nil, err
 	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(iUserID, iGroupID)
-
-	switch {
-	case err == sql.ErrNoRows:
-		//User does not exist
-		db.AddLogEvent(Log{Event: "GetUserGroupBookmarks_DontExists", Message: "Requested a nickname not present in the database", Error: err.Error()})
-		return nil, err
-
-	case err != nil:
-		db.AddLogEvent(Log{Event: "GetUserGroupBookmarks_DontExistsUnknown", Message: "Requested a nickname not present in the database but the error is unknown", Error: err.Error()})
-		return nil, err
-	}
-
-	var bkms []Bookmark
-
-	var id, userID, groupID, messageID, status int64
-	var messageContent, alias string
+	bkms := make([]Bookmark, 0)
 	for rows.Next() {
-		err = rows.Scan(&id, &userID, &groupID, &messageID, &alias, &status, &messageContent)
-		//And check for errors
-		if err != nil {
-			db.AddLogEvent(Log{Event: "GetUserGroupBookmarks_QueryScanError", Message: "An error verified while scanning a result row", Error: err.Error()})
+		var (
+			id, userID, groupID, messageID, status int64
+			messageContent, alias                  string
+		)
+		if err = rows.Scan(&id, &userID, &groupID, &messageID, &alias, &status, &messageContent); err != nil {
+			db.AddLogEvent(Log{Event: "GetUserGroupBookmarks_RowQueryFetchResultFailed", Message: "Impossible to get data from the row", Error: err.Error()})
 		} else {
 			bkms = append(bkms, Bookmark{ID: id, UserID: userID, GroupID: groupID, MessageID: messageID, Alias: alias, Status: status, MessageContent: messageContent})
 		}
 	}
+	if !rows.NextResultSet() {
+		db.AddLogEvent(Log{Event: "GetUserGroupBookmarks_RowNotFetched", Message: "Some rows in the query were not fetched", Error: err.Error()})
+	}
+	if err := rows.Err(); err != nil {
+		db.AddLogEvent(Log{Event: "GetUserGroupBookmarks_UnknowQueryError", Message: "An unknown error was thrown", Error: err.Error()})
+	}
+
 	return bkms, err
 }
