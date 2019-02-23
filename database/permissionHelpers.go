3e688148bda1a9d35305967cefbbb08d54bbda09
Andrea <togniand@gmail.com>
2019-02-23 15:39:12 +0100

    GetPermission returns int (not int64), added RemoveAllGroupPermissions, fixed SetPermissions query

--- database/permissionHelpers.go
+++ database/permissionHelpers.go
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS 'Permissions' (
 func (db *SQLiteDB) SetPermissions(prm Permission) error {
 
 	query, err := db.Exec("INSERT INTO Permissions (`UserID`, `GroupID` , `Permission`) VALUES (?,?,?) "+
-		"ON CONFLICT(`User`,`Group`,`Permission`) DO UPDATE "+
+		"ON CONFLICT(`UserID`,`GroupID`,`Permission`) DO UPDATE "+
 		"SET `Permission` = Excluded.Permission", prm.UserID, prm.GroupID, prm.Permission)
 	if err != nil {
 		db.AddLogEvent(Log{Event: "SetPermissions_QueryFailed", Message: "Impossible to create the execute the query", Error: err.Error()})
@@ -66,18 +66,70 @@ func (db *SQLiteDB) SetPermissions(prm Permission) error {
 	return errors.New("No changes to the database was made")*/
 }
 
+//RemoveAllGroupPermissions removes all the admins in a group
+func (db *SQLiteDB) RemoveAllGroupPermissions(groupID int64) error {
+
+	query, err := db.Exec("DELETE FROM Permissions WHERE `GroupID` =?", groupID)
+	if err != nil {
+		db.AddLogEvent(Log{Event: "RemoveAllGroupPermissions_QueryFailed", Message: "Impossible to create the execute the query", Error: err.Error()})
+		return err
+	}
+	rows, err := query.RowsAffected()
+	if err != nil {
+		db.AddLogEvent(Log{Event: "RemoveAllGroupPermissions_RowsInfoNotGot", Message: "Impossible to get afftected rows", Error: err.Error()})
+		return err
+	}
+	if rows < 1 {
+		db.AddLogEvent(Log{Event: "RemoveAllGroupPermissions_NoRowsAffected", Message: "No rows affected"})
+		return nil
+		// ACTUALLY, if the group has no permissions yet, it's ok to have0 results (but we still log 'em)
+		//return NoRowsAffected{error: errors.New("No rows affected from the query")}
+	}
+	return err
+}
+
 //GetPermission returns the permission of a user given its id and the group
-func (db *SQLiteDB) GetPermission(userID int64, groupID int64) (int64, error) {
+func (db *SQLiteDB) GetPermission(userID int64, groupID int64) (int, error) {
 	var perm int64
 	err := db.QueryRow("SELECT `Permission` FROM Permissions WHERE `ID`=?", "a").Scan(&perm)
 	switch {
 	case err == sql.ErrNoRows:
 		db.AddLogEvent(Log{Event: "GetPermission_ErrorNoRows", Message: "Impossible to get rows", Error: err.Error()})
-		return perm, err
+		return int(perm), nil
 	case err != nil:
 		db.AddLogEvent(Log{Event: "GetPermission_ErrorUnknown", Message: "Uknown error verified", Error: err.Error()})
-		return perm, err
+		return int(perm), nil
 	default:
-		return perm, nil
+		return int(perm), nil
 	}
+}
+
+//GetPrivilegedUsers returns an array of database.Permission type ofall the users in the database
+func (db *SQLiteDB) GetPrivilegedUsers(groupID int64) ([]Permission, error) {
+	rows, err := db.Query("SELECT `Permission` FROM Permissions WHERE `GroupID`=?", groupID)
+	defer rows.Close()
+	prms := make([]Permission, 0)
+	if err != nil {
+		db.AddLogEvent(Log{Event: "GetPrivilegedUsers_ErorExecutingTheQuery", Message: "Impossible to get afftected rows", Error: err.Error()})
+		return prms, err
+	}
+	for rows.Next() {
+		var (
+			id, userID, groupID, perm int64
+		)
+		if err = rows.Scan(&id, &userID, &groupID, &perm); err != nil {
+			db.AddLogEvent(Log{Event: "GetPrivilegedUsers_RowQueryFetchResultFailed", Message: "Impossible to get data from the row", Error: err.Error()})
+		} else {
+			prms = append(prms, Permission{ID: id, UserID: userID, GroupID: groupID, Permission: perm})
+		}
+	}
+	if err == sql.ErrNoRows {
+		return prms, nil
+	}
+	if rows.NextResultSet() {
+		db.AddLogEvent(Log{Event: "GetPrivilegedUsers_RowsNotFetched", Message: "Some rows in the query were not fetched"})
+	} else if err := rows.Err(); err != nil {
+		db.AddLogEvent(Log{Event: "GetPrivilegedUsers_UnknowQueryError", Message: "An unknown error was thrown", Error: err.Error()})
+	}
+	return prms, err
 }
