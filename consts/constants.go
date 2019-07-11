3c27ffd12d7ddb5d9644f51cf3a907e5126db99f
Andrea <togniand@gmail.com>
2019-07-11 20:30:54 +0200

    Version bumb

--- consts/constants.go
+++ consts/constants.go
@@ -6,7 +6,7 @@ package consts
 //Version is the bot version.
 //This number should be incremented for every release
 //Also, this shoud be inserted in the database and be incremented from it
-const Version = "0.2.0-g α"
+const Version = "0.2.1-g α"
 
 //DefaultLocale identifies the default locale of the bot
 const DefaultLocale = "it"
